540352f9fd893851d25aec87f1ea6b3a191d3db6
FelipeMDantas <felipemdantas@terra.com.br>
2023-05-05 22:49:55 -0300

    Connection to API established via Redux-Toolkit.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,49 @@
-import { linkIcon } from "../assets";
+import { useEffect, useState } from "react";
+import { copy, linkIcon } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("Articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
           <img
             src={linkIcon}
             alt="link_icon"
@@ -16,6 +55,8 @@ const Demo = () => {
             placeholder="Enter a URL"
             required
             className="url_input peer"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
           />
 
           <button
@@ -25,6 +66,25 @@ const Demo = () => {
             ↵
           </button>
         </form>
+
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p>{item.url}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
